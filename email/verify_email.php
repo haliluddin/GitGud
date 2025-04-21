a776be861389cddee55560998e23aa784b7909e6
haliluddin <hnailataji@gmail.com>
2025-04-21 18:39:30 +0800

    responsive email verification

--- email/verify_email.php
+++ email/verify_email.php
@@ -12,17 +12,17 @@
 ?>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet" href="../assets/css/style.css?v=<?php echo time(); ?>">
+<link rel="stylesheet" href="../assets/css/media.css?v=<?php echo time(); ?>">
 <style>
-    .whole{
+    .wholewhole{
         height: calc(100vh - 65.61px); 
         display: flex;
         justify-content: center;
         align-items: center;
     }
- 
 </style>
-<main>
-    <div class="whole bg-white border p-5 w-50 rounded-2 text-center">
+<main class="wholewhole">
+    <div class="bg-white border p-5 w-50 rounded-2 text-center">
         <img src="../assets/images/email.jpg" width="150" height="150">
         <h2 class="my-4">Verify your email address</h2>
         <p>A verification email has been sent to your email <span style="color: #CD5C08;"><?= $email ?></span><br>Please check your email and click the link provided in the email to complete your account registration.</p>
@@ -33,7 +33,7 @@
             <input type="hidden" name="user_id" value="<?= $_SESSION['user']['id'] ?>" />
             <input type="hidden" name="first_name" value="<?= $user['first_name'] ?>" />
             <input type="hidden" name="email" value="<?= $email ?>" />
-            <input type="submit" class="btn btn-primary w-50 p-3 rounded-5" value="Resend Verification Email" /> 
+            <input type="submit" class="btn btn-primary w-50 p-3 rounded-5 resever" value="Resend Verification Email" /> 
         </form>
     </div>
 
