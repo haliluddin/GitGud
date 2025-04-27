9c3335c2e9aa85dba8483208c5faf6a1f78c9167
haliluddin <hnailataji@gmail.com>
2025-04-27 20:35:31 +0800

    fixed footer alignment

--- footer.php
+++ footer.php
@@ -1,15 +1,15 @@
-<?php 
-    include_once 'links.php'; 
+<?php
+    include_once 'links.php';
 ?>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<div class="footer">
-    <div class="footer-top">
-        <div class="footer-about">
+<div class="site-footer">
+    <div class="site-footer__top">
+        <div class="site-footer__about">
             <h4>About Us</h4><br>
             <p>We offer a comprehensive platform designed to support food park owners and stall vendors alike. From seamless stall management to creating memorable guest experiences, our services help you thrive in a dynamic and fast-paced environment.</p>
         </div>
 
-        <div>
+        <div class="site-footer__links">
             <h4>Links</h4><br>
             <p><a href="">Terms & Conditions</a></p>
             <p><a href="">Privacy Policy</a></p>
@@ -17,14 +17,14 @@
             <p><a href="">Feedback</a></p>
         </div>
 
-        <div>
+        <div class="site-footer__contact">
             <h4>Contact Us</h4><br>
             <p>Zamboanga City, Philippines</p>
             <p>Email: gitgud@gmail.com</p>
             <p>Phone: +63 912 345 6789</p>
         </div>
 
-        <div>
+        <div class="site-footer__social">
             <h4>Follow Us</h4><br>
             <p><a href=""><i class="fab fa-facebook"></i>Facebook</a></p>
             <p><a href=""><i class="fab fa-instagram"></i>Instagram</a></p>
@@ -32,7 +32,7 @@
         </div>
     </div>
 
-    <div class="footer-bottom">
+    <div class="site-footer__bottom">
         <p>&copy; 2024 GitGud. All Rights Reserved.</p>
     </div>
 </div>
